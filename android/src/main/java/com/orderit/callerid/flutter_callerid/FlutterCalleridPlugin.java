32437a5c398fcc95e80967a9aa0db051ce75ee6e
yejia yang <yejia.yang@mycloudpa.com>
2025-07-17 13:07:38 +0100

    remove excetion

--- android/src/main/java/com/orderit/callerid/flutter_callerid/FlutterCalleridPlugin.java
+++ android/src/main/java/com/orderit/callerid/flutter_callerid/FlutterCalleridPlugin.java
@@ -45,11 +45,11 @@ public class FlutterCalleridPlugin implements FlutterPlugin, MethodCallHandler {
     public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
         switch (call.method) {
             case "getAvailableDevices":
-                try {
+//                try {
                     result.success(flutterCallerIdMethod.getUsbDevicesList());
-                } catch (Exception e) {
-                    result.error("GET_USB_FAILED", e.getMessage(), null);
-                }
+//                } catch (Exception e) {
+//                    result.error("GET_USB_FAILED", e.getMessage(), null);
+//                }
                 break;
             case "connectToHidDevice": {
                 String vendorId = call.argument("vendorId");
